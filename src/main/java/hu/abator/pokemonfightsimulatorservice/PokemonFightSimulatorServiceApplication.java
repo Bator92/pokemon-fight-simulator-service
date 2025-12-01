5c5c2126ff699d21e2d2f51d00276574e571b1b8
András Bátor <bator.andras@rsfx.hu>
2025-12-01 15:50:48 +0100

    Added essentials

---
+++ src/main/java/hu/abator/pokemonfightsimulatorservice/PokemonFightSimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.abator.pokemonfightsimulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonFightSimulatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonFightSimulatorServiceApplication.class, args);
+	}
+
+}
